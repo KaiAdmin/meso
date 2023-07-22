ad8bd4b00a0eb02b2051a28917cc980c1461a213
KaiAdmin <54000984+KaiAdmin@users.noreply.github.com>
2023-07-22 19:47:53 +0800

    second commit

--- cyzmalloc.cpp
+++ cyzmalloc.cpp
@@ -1,32 +1,65 @@
 #include<cmath>
-class HEAP{
+
+class Heap{
 public:
     int brk,chk;
-    int head=0,tot=0,nexts[33]={0};
-    void * initptr[33]={nullptr};
-    bool vis[33][128]={0};
+    int head=0,tot=0,nexts[3300]={0},last[3300]={0};
+    void * initptr[3300]={nullptr};
+    bool vis[3300][128]={0};
+    int viscnt[3300]={0};
 }h[16];
+
 void * cyzmalloc(size_t size){
-    int g=ceil(log2(size));
+    int mlcsiz=ceil(log2(size));
     //if size is too large
-    if(g>=17)return malloc(size);
+    if(mlcsiz>=17)return malloc(size);
+
     //allocate
-    if(h[g].tot!=0){
-        for(int i=h[g].head;i;i=h[g].nexts[i]){
+    if(h[mlcsiz].tot!=0){
+        for(int i=h[mlcsiz].head;i;i=h[mlcsiz].nexts[i]){
             for(int j=0;j<128;j++){
-                if(h[g].vis[i][j]==0){
-                    h[g].vis[i][j]=1;
-                    return (void*)h[g].initptr[i]+h[g].chk*j;
+                if(h[mlcsiz].vis[i][j]==0){
+                    h[mlcsiz].vis[i][j]=1;h[mlcsiz].viscnt[i]++;
+                    return (void*)h[mlcsiz].initptr[i]+h[mlcsiz].chk*j;
                 }   
             }
         }
     }
+
     //if used up
-    if(h[g].tot<=32){
-        h[g].initptr[++h[g].tot]=malloc((1<<g)*128);h[g].nexts[h[g].tot]=h[g].head;h[g].head=h[g].tot;
-        h[g].vis[h[g].tot][0]=1;
-        return (void *)h[g].initptr[h[g].tot];
+    if(h[mlcsiz].tot<=3200){
+        h[mlcsiz].initptr[++h[mlcsiz].tot]=malloc((1<<mlcsiz)*128);
+
+        h[mlcsiz].nexts[h[mlcsiz].tot]=h[mlcsiz].head;h[mlcsiz].head=h[mlcsiz].tot;
+
+        h[mlcsiz].last[h[mlcsiz].nexts[h[mlcsiz].tot]]=h[mlcsiz].tot;
+
+        h[mlcsiz].vis[h[mlcsiz].tot][0]=1;h[mlcsiz].viscnt[h[mlcsiz].tot]++;
+
+        return (void *)h[mlcsiz].initptr[h[mlcsiz].tot];
     }
     //then fail
     return nullptr;
+}
+
+void cyzfree(void * ptr){
+    for(int i=1;i<=16;i++){//which h
+        for(int j=h[i].head;j;j=h[i].nexts[j]){
+            if(h[i].initptr[j]<=ptr&&ptr<h[i].initptr[j]+h[i].brk){//find
+                if(((ptrdiff_t)ptr-(ptrdiff_t)h[i].initptr[j])%h[i].chk!=0)return;//not available
+                if(h[i].vis[j][((ptrdiff_t)ptr-(ptrdiff_t)h[i].initptr[j])/h[i].chk]==0)return;//it has been freed
+
+                h[i].vis[j][((ptrdiff_t)ptr-(ptrdiff_t)h[i].initptr[j])/h[i].chk]=0;//free
+                h[i].viscnt[j]--;
+
+                if(h[i].viscnt[j]==0){
+                    free(h[i].initptr[j]);//free this space;
+                    h[i].last[h[i].nexts[j]]=h[i].last[j];
+                    h[i].nexts[h[i].last[j]]=h[i].nexts[j];
+                }
+
+                return;
+            }
+        }
+    }
 }
